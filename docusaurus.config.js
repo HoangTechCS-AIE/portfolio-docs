de44c962f3dd6106e1ebd2192e412377a3befb1d
HoangTechCS-AIE <nguyenhuyhoangqbx5@gmail.com>
2025-12-18 11:23:20 +0700

    Fix baseUrl for Vercel deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
     url: 'https://HoangTechCS-AIE.github.io',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/portfolio-docs/',
+    baseUrl: '/',
 
     // GitHub pages deployment config.
     organizationName: 'HoangTechCS-AIE', // Usually your GitHub org/user name.
